4454c58c0e77604e21203963df63f8a7fedb6d2b
unel <unel@bk.ru>
2017-11-08 20:05:31 +0300

    add typings.. (in progress)

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import {types} from 'mobx-state-tree';
+import {observer} from 'mobx-react';
 
 const ReminderCategory = types.model("ReminderCategory", {
   title: types.string,
@@ -33,12 +34,41 @@ const styles = StyleSheet.create({
   }
 });
 
+@observer
+class RemindersList extends React.Component {
+  render() {
+    const store = this.props.store;
+
+    return (
+      <View style={[styles.container, {flex: 3, borderColor: 'red', borderStyle: 'solid', borderWidth: 1}]}>
+        <Text style={styles.text}>kokoko</Text>
+        {store.reminders.map(reminder => (
+          <Text style={styles.text}>{reminder.title}</Text>
+        ))}
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this._store = Store.create({
+      reminders: [
+        {title: "rm1", category: {title: "a", remindOptions: {notificationType: "z"}}},
+        {title: "rm2", category: {title: "b", remindOptions: {notificationType: "z"}}}
+      ]
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Open up App.js to start working on your app!!</Text>
+        <View style={styles.container}>
+          <Text style={styles.text}>Open up App.js to start working on your app!!</Text>
+        </View>
+        <RemindersList store={store}/>
       </View>
     );
   }
