7a830dc7e48a7e753f35df31fb6db7d6e9b69398
unel <unel@bk.ru>
2017-11-08 19:12:47 +0300

    reminder initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {types} from 'mobx-state-tree';
+
+const ReminderCategory = types.model("ReminderCategory", {
+  title: types.string,
+  remindOptions: types.compose({
+    notificationType: type.string
+  })
+})
+
+const Reminder = types.model("Reminder", {
+  title: types.string,
+  category: types.reference("ReminderCategory")
+});
+
+const Store = types.model({
+  reminders: types.array(Reminder)
+});
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: 'white',
+  }
+});
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!!</Text>
+      </View>
+    );
+  }
+}
